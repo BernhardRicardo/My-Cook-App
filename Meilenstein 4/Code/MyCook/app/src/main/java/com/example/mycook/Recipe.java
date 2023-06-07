2e8a111cfd9971d97d7a6b41e1425c00d70c4427
strnkoeh <robin.c.koehl@stud.h-da.de>
2023-06-07 17:49:45 +0200

    Fixes design

--- Meilenstein 4/Code/MyCook/app/src/main/java/com/example/mycook/Recipe.java
+++ Meilenstein 4/Code/MyCook/app/src/main/java/com/example/mycook/Recipe.java
@@ -1,14 +1,43 @@
 package com.example.mycook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Recipe extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        //Menu-Bar
+        bottomNavigationView = findViewById(R.id.bottonnav);
+        bottomNavigationView.setSelectedItemId(R.id.bottonnav);
+        //Function deprecated--> maybe switch in future
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.inventory) {
+                    startActivity(new Intent(getApplicationContext(), InventoryList.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                } else if (item.getItemId() == R.id.shopping){
+                    startActivity(new Intent(getApplicationContext(), ShoppingList.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                } else
+                    return false;
+            }
+        }
+        );
     }
-}
+    }
