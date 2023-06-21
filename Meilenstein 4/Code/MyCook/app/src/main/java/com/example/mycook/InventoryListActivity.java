d8ae4c63929c06159c7ad76993dd267d929a29e9
strnkoeh <robin.c.koehl@stud.h-da.de>
2023-06-21 04:08:56 +0200

    Fixed shoppinglist and inventory interaction. you can add items to inventory now. inventory now gets items from a file

--- Meilenstein 4/Code/MyCook/app/src/main/java/com/example/mycook/InventoryListActivity.java
+++ Meilenstein 4/Code/MyCook/app/src/main/java/com/example/mycook/InventoryListActivity.java
@@ -12,19 +12,28 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class InventoryListActivity extends AppCompatActivity {
+   //Inventory File
+   private static String FILE_PATH = "/data/data/com.example.mycook/files/testInventory.txt";
 
+    private static String FILE_NAME = "testInventory.txt";
     ListView invList;
-    static String inventar[] = {
-            "Nudeln", "Mozarella", "Wodka", "Pierogi"
-    };
+    ArrayAdapter<String> invAdapter;
+    ArrayList<String> inventarArrayList = new ArrayList<String>();
 
     //ArrayList<String> inventar = getIntent().getStringArrayListExtra("inventar");
-
 
     BottomNavigationView bottomNavigationView;
     @Override
@@ -40,44 +49,113 @@ public class InventoryListActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.b_shopping) {
+                    updateInventoryData();
                     startActivity(new Intent(getApplicationContext(), ShoppingListActivity.class));
                     overridePendingTransition(0,0);
                     return true;
                 } else if (item.getItemId() == R.id.b_favorites){
+                    updateInventoryData();
                     startActivity(new Intent(getApplicationContext(), FavoritesActivity.class));
                     overridePendingTransition(0,0);
                     return true;
                 } else
                     return false;
+
+
             }
         }
         );
 
-        invList = findViewById(R.id.listInventar);
+       /* invList = findViewById(R.id.listInventar);
+        inventarArrayList.add("Onion");
         // inventar = new ArrayList<>();
-        ArrayAdapter<String> arr;
-        arr = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, inventar);
-        invList.setAdapter(arr);
+        ArrayAdapter<String> inventarAdapter;
+        inventarAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, inventarArrayList);
+        invList.setAdapter(inventarAdapter);*/
 
-        invList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//Checks if File exits
+        File f = new File(FILE_PATH);
+        //Opens file if existing
+        if (f.exists() == true) {
+            FileInputStream fis = null;
+            try {
+                fis = openFileInput(FILE_NAME);
+                InputStreamReader isr = new InputStreamReader(fis);
+
+                BufferedReader br = new BufferedReader(isr);
+                StringBuilder sb = new StringBuilder();
+                String text;
+
+                while ((text = br.readLine()) != null) {
+                    inventarArrayList.add(text);
+                }
+
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException("FILE NOT FOUND");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            invList = findViewById(R.id.listInventar);
+            invAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, inventarArrayList);
+            invList.setAdapter(invAdapter);
+        }
+        else {
+            //No Inventory file
+            Toast.makeText(this, "Nothing in your inventory!", Toast.LENGTH_LONG).show();
+            invList = findViewById(R.id.listInventar);
+            invAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, inventarArrayList);
+            invList.setAdapter(invAdapter);
+        }
+       invList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView parent, View view, int position, long id) {
                 // Entferne das Element aus der Liste
 
-                // inventar.remove(position); // gehört zu ArrayList
-                //löschen von Array namens inventar
-                int positionToDelete = position;
-                for (int i = positionToDelete; i < inventar.length - 1; i++) {
-                    inventar[i] = inventar[i + 1];
-                }
-                inventar = Arrays.copyOf(inventar, inventar.length - 1);
-                // Aktualisiere die ListView
-                arr.notifyDataSetChanged();
+                inventarArrayList.remove(position); // gehört zu ArrayList
+
+                //Aktualisiere Adapter
+                invAdapter.notifyDataSetChanged();
 
                 return true;
             }
         });
+    }
+    //Updates Inventory file after deleting stuff
+    public void updateInventoryData(){
+        String selectedItem = "";
+        for (int i = 0; i < invList.getCount(); i++) {
+                selectedItem += invList.getItemAtPosition(i) + "\n";
 
+        }
+        //writes into inventory file
+        FileOutputStream fos = null;
+        String text = selectedItem.toString();
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (fos != null) {
+                try {
+                    //close file
+                    fos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
     }
 
 }
--- Meilenstein 4/Code/MyCook/app/src/main/java/com/example/mycook/ShoppingListActivity.java
+++ Meilenstein 4/Code/MyCook/app/src/main/java/com/example/mycook/ShoppingListActivity.java
@@ -15,18 +15,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import android.view.View.OnClickListener;
 
 public class ShoppingListActivity extends AppCompatActivity {
 
-
+    private static String FILE_NAME = "testInventory.txt";
     EditText eingabeText;
+    Button addInventoryButton;
     Button addButton;
     ListView shoppingListView;
-    ArrayAdapter <String> adapterShopping;
-    ArrayList<String> inventar;
+    ArrayAdapter<String> adapterShopping;
+    ArrayList<String> shoppingItems;
     BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,14 +42,15 @@ public class ShoppingListActivity extends AppCompatActivity {
         bottomNavigationView.setSelectedItemId(R.id.b_shopping);
         //Function deprecated--> maybe switch in future
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.b_inventory) {
                     startActivity(new Intent(getApplicationContext(), InventoryListActivity.class));
-                    overridePendingTransition(0,0);
+                    overridePendingTransition(0, 0);
                     return true;
-                } else if (item.getItemId() == R.id.b_favorites){
+                } else if (item.getItemId() == R.id.b_favorites) {
                     startActivity(new Intent(getApplicationContext(), FavoritesActivity.class));
-                    overridePendingTransition(0,0);
+                    overridePendingTransition(0, 0);
                     return true;
                 } else
                     return false;
@@ -55,24 +61,24 @@ public class ShoppingListActivity extends AppCompatActivity {
         eingabeText = findViewById(R.id.eingabefeldShoppinglist);
         addButton = findViewById(R.id.buttonAddShoppinglist);
         shoppingListView = findViewById(R.id.listviewShoppinglist);
-
-        inventar = new ArrayList<>();
-        adapterShopping = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, inventar);
+        addInventoryButton = findViewById(R.id.addItems);
+        shoppingItems = new ArrayList<>();
+        adapterShopping = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, shoppingItems);
         shoppingListView.setAdapter(adapterShopping);
 
         Intent intent = new Intent(this, InventoryListActivity.class);
-        intent.putStringArrayListExtra("inventar", inventar);
+        intent.putStringArrayListExtra("inventar", shoppingItems);
 
         addButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Füge das eingegebene Element zur Liste hinzu
                 String newItem = eingabeText.getText().toString();
-                inventar.add(newItem);
+                shoppingItems.add(newItem);
                 // Aktualisiere den Adapter
                 adapterShopping.notifyDataSetChanged();
                 // Setze das Eingabefeld zurück
-                eingabeText.setText("WarenName");
+                eingabeText.setText("");
 
 
             }
@@ -82,12 +88,49 @@ public class ShoppingListActivity extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 // Entferne das Element aus der Liste
-                inventar.remove(position);
+                shoppingItems.remove(position);
                 // Aktualisiere den Adapter
                 adapterShopping.notifyDataSetChanged();
                 return true;
             }
         });
+        addInventoryButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String selectedItem = "";
+                for (int i = 0; i < shoppingListView.getCount(); i++) {
+                    if (shoppingListView.isItemChecked(i)) {
+                        selectedItem += shoppingListView.getItemAtPosition(i) + "\n";
 
+                        //Deletes items. wich have been added to the inventory
+                        shoppingItems.remove(i);
+                        adapterShopping.notifyDataSetChanged();
+                    }
+                }
+                //writes into inventory file
+                FileOutputStream fos = null;
+                String text = selectedItem.toString();
+
+                try {
+                    fos = openFileOutput(FILE_NAME, MODE_APPEND);
+                    fos.write(text.getBytes());
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    if (fos != null) {
+                        try {
+                            //close file
+                            fos.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                }
+            }
+        });
     }
 }
+
